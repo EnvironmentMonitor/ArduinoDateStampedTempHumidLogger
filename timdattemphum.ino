47e230ecab3d0841840513e2ba07e73e4c496d1c
Environmental Logging Equipment OPEN SOURCE <environmantalmonintors@gmail.com>
2015-05-03 01:17:48 +0100

    Update timdattemphum.ino
    Removed Need for 2 Libraries.....to save memory.....

--- timdattemphum.ino
+++ timdattemphum.ino
@@ -16,71 +16,109 @@ This is a just one Reliable approach to IOT Logging, put it on the SDCARD then
 PUSH The Data to the Remote Server Seperately allowing Local Analyssis and backup.
 
 Use Arduino UNO or Mini Pro --> 
-Ethernet Shield or SDCARD Breakout Board (With 8GB SDCARD) --> 
+Ethernet Shield or SDCARD Breakout Board With 8GB SDCARD (Min=2Gb) --> SPI Pins 10-13 + 4
 DS18B20 (As many as the Topology Algorithm Allows) --> Data pin Attached to Arduino Pin 3
-DHT22 First Sensor --> Data Pin Attached to Arduino Pin 7
-DHT22 Second Sensor --> Data Pin Attached to Arduino Pin 8
-The Pattern is repeated per DHT22 Add additional pins.......
-The DS1307 RTC gives Time & Date via the I2CDev Bus, This Also has an additional DS18B20
+DHT22 First Sensor --> Data Pin Attached to Arduino Pin 2
+DHT22 Second Sensor --> Data Pin Attached to Arduino Pin 5
+The Pattern is repeated per DHT22 Add additional pins, Data Pins 2,3,5 NEED 10K Pullups !!!
+The DS1307 RTC gives Time & Date via the I2C Bus, This Also has an additional DS18B20
 Location onboard and can be used to Join the Sensors to the Arduino, Timestamps on SDWrite.
-The Files are CSV tab seperated, Datalog.CSV and Humidlog.CSV on the SDCARD, if the Files
+The Files are CSV Comma seperated, Datalog.CSV and Humidlog.CSV on the SDCARD, if the Files
 Exist they will be Appended if not they will be Created, A Valid Partition IS NEEDED on SD.
-Datalog.CSV
-The Sensor ID for the Temperature Currently only LOGS limited fields to expand use more
-Elements from the address array.
+Datalog.CSV   ----   DS18B20 Log
+The Sensor ID for the Temperature Currently only LOGS limited fields, to expand use more
+Elements from the address array(noted below).
 Temperature in Centigrade, there is also the option of Temperatures in Fahrenheit..
-Humidlog.CSV
-The Sensor ID is from the Text Below and the Temperature in Centigrade along with
-the Relative Humidity in Percentage are logged from each sensor,
+Humidlog.CSV  ----   DHT22 Log
+The Sensor ID is from the Text Below(S1,S2) and the Temperature in Centigrade along with
+the Relative Humidity in Percentages are logged from each sensor,
 there is also A Heat Index that can be used along with Temperatures in Fahrenheit.
 This is running most of the Arduino Memory but there is enough left to load HTTP
 files from SD to process in one routine but this can be Unreliable due to Timing of HTTP.
-
-!*!*! Please note the Libraries Required in the Includes.......
-
+________________________________________________________________________________________
+!*!*!*!*!*!*!* ALL Data is sent to the Serial Console with Time Stamps  *!*!*!*!*!*!*!*!
+___________________________________________________________________________
+!*!*!*! Please note the 4 Libraries Required in the Includes.......!*!*!*!|
+---------------------------------------------------------------------------
 */
-
-#include "Wire.h"
-#include "I2Cdev.h"
-#include "DS1307.h"
-#include "DHT.h"
+#include <Wire.h>
+#include <DHT.h>
 #include <OneWire.h>
 #include <SD.h>
-
-#define DHTPIN 7     // what pin we're connected to for the Humidity Sensors
-#define DHTPIN1 8
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
+//_________________________________________________________________________________________
+//!*!*! The Libraries MUST be installed Before this will Compile^You have been Warned !*!*!
+//_________________________________________________________________________________________
+#define DS1307_I2C_ADDRESS 0x68
+#define DHTPIN 2               // What pin we're connected to for the First Humidity Sensor
+#define DHTPIN1 5              // What pin we're connected to for the Second Humidity Sensor
+#define DHTTYPE DHT22          // DHT 22  (AM2302)
 // Initialize DHT sensor for normal 16mhz Arduino
 DHT dht(DHTPIN, DHTTYPE);
 DHT dht1(DHTPIN1, DHTTYPE);
 // Initialize DHT sensor for 84mhz Arduino DUE
 //DHT dht(DHTPIN, DHTTYPE, 30);
-// Setup Real Time Clock, the time IN DS1307 MUST BE SET CORRECTLY !!!
-DS1307 rtc;
-uint16_t year;
-uint8_t month, day, hours, minutes, seconds;
 OneWire  ds(3);  // DS18B20 OneWire Data Pin on Arduino pin 3 For the Temperature Sensors
 const int chipSelect = 4;      // CS for SD card
-
-void setup() {
-  // read all clock info from device
-  rtc.getDateTime24(&year, &month, &day, &hours, &minutes, &seconds);
-  Serial.begin(9600); 
-  Serial.print("Currently Set Time Is :- ");
-  Serial.print(year);
-  Serial.print( "/");
-  Serial.print(month);
-  Serial.print("/");
-  Serial.print(day);
-  Serial.print("|");
-  Serial.print(hours);
-  Serial.print(":");
-  Serial.print(minutes);
-  Serial.print(":");
-  Serial.println(seconds);
-  Serial.println("DHTxx test!");
-  Serial.print("Initializing SD card...");
-  pinMode(10, OUTPUT);
+byte decToBcd(byte val)
+{
+ return ( (val/10*16) + (val%10) );
+}
+byte bcdToDec(byte val)
+{
+ return ( (val/16*10) + (val%16) );
+}
+// Assumes you're passing in valid numbers
+void setDateDs1307(byte second, // 0-59
+ byte minute, // 0-59
+ byte hour, // 1-23
+ byte dayOfWeek, // 1-7
+ byte dayOfMonth, // 1-28/29/30/31
+ byte month, // 1-12
+ byte year) // 0-99
+{
+ Wire.beginTransmission(DS1307_I2C_ADDRESS);
+ Wire.write(0);
+ Wire.write(decToBcd(second)); // 0 to bit 7 starts the clock
+ Wire.write(decToBcd(minute));
+ Wire.write(decToBcd(hour));
+ Wire.write(decToBcd(dayOfWeek));
+ Wire.write(decToBcd(dayOfMonth));
+ Wire.write(decToBcd(month));
+ Wire.write(decToBcd(year));
+ Wire.endTransmission();
+}
+// Gets the date and time from the ds1307
+void getDateDs1307(byte *second,
+ byte *minute,
+ byte *hour,
+ byte *dayOfWeek,
+ byte *dayOfMonth,
+ byte *month,
+ byte *year)
+{
+// Reset the register pointer
+ Wire.beginTransmission(DS1307_I2C_ADDRESS);
+ Wire.write(0);
+ Wire.endTransmission();
+ Wire.requestFrom(DS1307_I2C_ADDRESS, 7);
+// A few of these need masks because certain bits are control bits
+ *second = bcdToDec(Wire.read() & 0x7f);
+ *minute = bcdToDec(Wire.read());
+ *hour = bcdToDec(Wire.read() & 0x3f); // Need to change this if 12 hour am/pm
+ *dayOfWeek = bcdToDec(Wire.read());
+ *dayOfMonth = bcdToDec(Wire.read());
+ *month = bcdToDec(Wire.read());
+ *year = bcdToDec(Wire.read());
+}
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("Starting The Environmental Monitor ...");
+    Serial.println("Logging All Sensor Readings to SD card ...");
+    Serial.println("DHT22 Humidity & Temperature Sensors (*2) plus ...");
+    Serial.println("DS18B20 Temperature Sensors (Min = 1) with DS1307 RTC ...");  
+    Serial.print("Initializing SD card...");
+    pinMode(10, OUTPUT);
   if (!SD.begin(chipSelect)) {
     Serial.println("Card failed, or not present");
     return;
@@ -88,12 +126,23 @@ void setup() {
   Serial.println("card initialized.");
   dht.begin();
   dht1.begin();
+ byte second, minute, hour, dayOfWeek, dayOfMonth, month, year;
+ second = 10;
+ minute = 01;
+ hour = 15;
+ dayOfWeek = 1;  // First Day "Saturday"
+ dayOfMonth = 2;
+ month = 5;
+ year = 15;
+// Set the correct time and date Above and use the line below to set the time if Needed!
+// setDateDs1307(second, minute, hour, dayOfWeek, dayOfMonth, month, year);
 }
 
-void loop() {
-  // Wait a couple of seconds between measurements.
-  delay(2000);
- byte i;
+void loop()
+{
+ byte second, minute, hour, dayOfWeek, dayOfMonth, month, year;
+ getDateDs1307(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month, &year);
+  byte i;
   byte present = 0;
   byte type_s;
   byte data[12];
@@ -111,12 +160,40 @@ void loop() {
     Serial.write(' ');
     Serial.print(addr[i], HEX);
   }
+  Serial.println();
   if (OneWire::crc8(addr, 7) != addr[7]) {
-      Serial.println("CRC is not valid!");
+      Serial.print("CRC is not valid!");
+      getDateDs1307(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month, &year);
+       Serial.print(hour, DEC);
+       Serial.print(":");
+       Serial.print(minute, DEC);
+       Serial.print(":");
+       Serial.print(second, DEC);
+       Serial.print(" ");
+       Serial.print(month, DEC);
+       Serial.print("/");
+       Serial.print(dayOfMonth, DEC);
+       Serial.print("/");
+       Serial.print(year, DEC);
+       Serial.print(" Day_of_week:");
+       Serial.println(dayOfWeek, DEC);
       return;
   }
-  Serial.println();
-  // the first ROM byte indicates which chip
+  getDateDs1307(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month, &year);
+ Serial.print(hour, DEC);
+ Serial.print(":");
+ Serial.print(minute, DEC);
+ Serial.print(":");
+ Serial.print(second, DEC);
+ Serial.print(" ");
+ Serial.print(month, DEC);
+ Serial.print("/");
+ Serial.print(dayOfMonth, DEC);
+ Serial.print("/");
+ Serial.print(year, DEC);
+ Serial.print(" Day_of_week:");
+ Serial.println(dayOfWeek, DEC);
+// the first ROM byte indicates which chip
   switch (addr[0]) {
     case 0x10:
       Serial.println("  Chip = DS18S20");  // or old DS1820
@@ -134,22 +211,17 @@ void loop() {
       Serial.println("Device is not a DS18x20 family device.");
       return;
   } 
-
   ds.reset();
   ds.select(addr);
-  ds.write(0x44,1);         // start conversion, with parasite power on at the end
-  
-  delay(1000);     // maybe 750ms is enough, maybe not
-  // we might do a ds.depower() here, but the reset will take care of it.
-  
+  ds.write(0x44,1);                       // start conversion, with parasite power on at the end
+  delay(1000);                            // maybe 750ms is enough, maybe not
   present = ds.reset();
   ds.select(addr);    
-  ds.write(0xBE);         // Read Scratchpad
-
+  ds.write(0xBE);                         // Read Scratchpad
   Serial.print("  Data = ");
   Serial.print(present,HEX);
   Serial.print(" ");
-  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+  for ( i = 0; i < 9; i++) {              // we need 9 bytes
     data[i] = ds.read();
     Serial.print(data[i], HEX);
     Serial.print(" ");
@@ -157,9 +229,7 @@ void loop() {
   Serial.print(" CRC=");
   Serial.print(OneWire::crc8(data, 8), HEX);
   Serial.println();
-
-  // convert the DS18B20 Hex data to actual temperature
-
+ // convert the DS18B20 Hex data to actual temperature
   unsigned int raw = (data[1] << 8) | data[0];
   if (type_s) {
     raw = raw << 3; // 9 bit resolution default
@@ -172,91 +242,96 @@ void loop() {
     if (cfg == 0x00) raw = raw << 3;  // 9 bit resolution, 93.75 ms
     else if (cfg == 0x20) raw = raw << 2; // 10 bit res, 187.5 ms
     else if (cfg == 0x40) raw = raw << 1; // 11 bit res, 375 ms
-    // default is 12 bit resolution, 750 ms conversion time
+// default is 12 bit resolution, 750 ms conversion time
   }
   celsius = (float)raw / 16.0;
   fahrenheit = celsius * 1.8 + 32.0;
-  rtc.getDateTime24(&year, &month, &day, &hours, &minutes, &seconds);
+  
+ getDateDs1307(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month, &year);  
   File dataFile = SD.open("datalog.CSV", FILE_WRITE);
-    // if the file is available, write to it:
+// if the file is available, write to it:
     if (dataFile) {
-      dataFile.print("  Sensor ID = ");
-      dataFile.print(addr[7],HEX);  //  <--- insert additonal Address data here
-      dataFile.print("  Temperature = ");
-      dataFile.print(celsius);
-      dataFile.print(" Celsius ");
-  dataFile.print(year);
-  dataFile.print("/");
-  dataFile.print(month);
-  dataFile.print("/");
-  dataFile.print(day);
-  dataFile.print("|");
-  dataFile.print(hours);
-  dataFile.print(":");
-  dataFile.print(minutes);
-  dataFile.print(":");
-  dataFile.println(seconds);
-      dataFile.close();
-//    delay(5000);// Pause here to ensure when power off FILE CLOSED!!
+ dataFile.print("  Sensor ID = ,");
+ dataFile.print(addr[7],HEX);  //  <--- insert additonal Address field data here [...6,7]
+ dataFile.print(",  Temperature = ,");
+ dataFile.print(celsius);
+ dataFile.print(", Celsius ");
+ dataFile.print(" Date Stamp ,");
+ dataFile.print(hour, DEC);
+ dataFile.print(":");
+ dataFile.print(minute, DEC);
+ dataFile.print(":");
+ dataFile.print(second, DEC);
+ dataFile.print(" ");
+ dataFile.print(month, DEC);
+ dataFile.print("/");
+ dataFile.print(dayOfMonth, DEC);
+ dataFile.print("/");
+ dataFile.print(year, DEC);
+ dataFile.print(" Day_of_week:");
+ dataFile.print(dayOfWeek, DEC); 
+ dataFile.println(",");
+ dataFile.close();
+    delay(5000);// Pause here to ensure when power off FILE CLOSED!!
       }         // Wait for Pin 13 LED (SPI Activity) to Flash before power off!
-    // if the file isn't open, pop up an error:
+// if the file isn't open, pop up an error:
     else {
-      Serial.println("error opening datalog.CSV");
+return;
     }
-  rtc.getDateTime24(&year, &month, &day, &hours, &minutes, &seconds);
   float h = dht.readHumidity();
-  // Read temperature as Celsius
+// Read temperature as Celsius
   float t = dht.readTemperature();
-  // Read temperature as Fahrenheit
+// Read temperature as Fahrenheit
   float f = dht.readTemperature(true);
   if (isnan(h) || isnan(t) || isnan(f)) {
-    Serial.println("Failed to read from DHT sensor!");
     return;
   }
   float hi = dht.computeHeatIndex(f, h);
   float h1 = dht1.readHumidity();
-  // Read temperature as Celsius
+// Read temperature as Celsius
   float t1 = dht1.readTemperature();
-  // Read temperature as Fahrenheit
+// Read temperature as Fahrenheit
   float f1 = dht1.readTemperature(true);
   if (isnan(h1) || isnan(t1) || isnan(f1)) {
-    Serial.println("Failed to read from DHT sensor!");
     return;
   }
   float hi1 = dht1.computeHeatIndex(f1, h1);
+ getDateDs1307(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month, &year);  
       File dataFile1 = SD.open("humidlog.CSV", FILE_WRITE);
-    // if the file is available, write to it:
+// if the file is available, write to it:
     if (dataFile1) {
-      dataFile1.print("  S1 Humidity Percentage = ");
-      dataFile1.print(h);
-      dataFile1.print("  S1 Temperature = ");
-      dataFile1.print(t);
-      dataFile1.println(" Celsius ");
-      dataFile1.print("  S2 Humidity Percentage = ");
-      dataFile1.print(h1);
-      dataFile1.print("  S2 Temperature = ");
-      dataFile1.print(t1);
-      dataFile1.print(" Celsius ");
-  dataFile1.print(year);
-  dataFile1.print("/");
-  dataFile1.print(month);
-  dataFile1.print("/");
-  dataFile1.print(day);
-  dataFile1.print("|");
-  dataFile1.print(hours);
-  dataFile1.print(":");
-  dataFile1.print(minutes);
-  dataFile1.print(":");
-  dataFile1.println(seconds);
-      dataFile1.close();
+ dataFile1.print("  S1 Humidity Percentage = ,");
+ dataFile1.print(h);
+ dataFile1.print(",  S1 Temperature = ,");
+ dataFile1.print(t);
+ dataFile1.print(", Celsius ");
+ dataFile1.print("  S2 Humidity Percentage = ,");
+ dataFile1.print(h1);
+ dataFile1.print(",  S2 Temperature = ,");
+ dataFile1.print(t1);
+ dataFile1.print(", Celsius ,");
+ dataFile1.print(hour, DEC);
+ dataFile1.print(":");
+ dataFile1.print(minute, DEC);
+ dataFile1.print(":");
+ dataFile1.print(second, DEC);
+ dataFile1.print(" ");
+ dataFile1.print(month, DEC);
+ dataFile1.print("/");
+ dataFile1.print(dayOfMonth, DEC);
+ dataFile1.print("/");
+ dataFile1.print(year, DEC);
+ dataFile1.print(" Day_of_week:");
+ dataFile1.print(dayOfWeek, DEC);
+ dataFile1.println(",");
+ dataFile1.close();
     delay(5000);// Pause here to ensure when power off FILE CLOSED!!
       }         // Wait for Pin 13 LED (SPI Activity) to Flash before power off!
-    // if the file isn't open, pop up an error:
+// if the file isn't open, pop up an error:
     else {
-      Serial.println("error opening humidlog.CSV");
+      return;
     }
-  rtc.getDateTime24(&year, &month, &day, &hours, &minutes, &seconds);
-  Serial.print("Humidity: "); 
+  Serial.print("First - Humidity: "); 
   Serial.print(h);
   Serial.print(" %\t");
   Serial.print("Temperature: "); 
@@ -266,17 +341,31 @@ void loop() {
   Serial.print(" *F\t");
   Serial.print("Heat index: ");
   Serial.print(hi);
+  Serial.print(" *F");
+  Serial.print("Second - Humidity: "); 
+  Serial.print(h1);
+  Serial.print(" %\t");
+  Serial.print("Temperature: "); 
+  Serial.print(t1);
+  Serial.print(" *C ");
+  Serial.print(f1);
+  Serial.print(" *F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hi1);
   Serial.println(" *F");
-  Serial.print("Currently Set Time Is :- ");
-  Serial.print(year);
-  Serial.print( "/");
-  Serial.print(month);
-  Serial.print("/");
-  Serial.print(day);
-  Serial.print("|");
-  Serial.print(hours);
-  Serial.print(":");
-  Serial.print(minutes);
-  Serial.print(":");
-  Serial.println(seconds);
+ getDateDs1307(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month, &year);
+ Serial.print(hour, DEC);
+ Serial.print(":");
+ Serial.print(minute, DEC);
+ Serial.print(":");
+ Serial.print(second, DEC);
+ Serial.print(" ");
+ Serial.print(month, DEC);
+ Serial.print("/");
+ Serial.print(dayOfMonth, DEC);
+ Serial.print("/");
+ Serial.print(year, DEC);
+ Serial.print(" Day_of_week:");
+ Serial.println(dayOfWeek, DEC);
+ delay(10);
 }
